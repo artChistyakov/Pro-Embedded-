4e66a7bd7d8283b38a97a60ed3d7f50cc89e9eb5
artChistyakov <tomsonone08@gmail.com>
2026-02-28 00:04:46 +0200

    Lesson 22 task 3 (Pin touched)

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <Arduino.h>
-unsigned long int timer;
-
+int threshold = 11;
+int touchValue;
+bool isReleased = true;
 
 void setup() {
   Serial.begin(115200);
-  timer = millis();
 }
 
 void loop() {
-  if (millis() -  timer >= 100) {
-    Serial.println("Touch value:" + (String)touchRead(4));
-    timer = millis();
-  }
+ touchValue = touchRead(4);
+
+ if (touchValue <= threshold && isReleased) {
+  Serial.println("Pin touched");
+  isReleased = false;
+ } else if (touchValue > threshold && !isReleased) {
+  Serial.println("Pin released");
+  isReleased = true;
+ }
 }
 
